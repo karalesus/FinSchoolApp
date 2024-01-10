c51ab4c35702ccf97353f94316d68e0d34e64998
karalesus <karalesus@gmail.com>
2024-01-11 01:16:39 +0400

    first commit

---
+++ app/src/main/java/com/example/finschoolapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.finschoolapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
