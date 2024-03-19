2de5ac67fb7fc7d9725b7c2be3e3eb27821ebf64
karalesus <karalesus@gmail.com>
2024-03-19 14:57:44 +0300

    configure Kotlin in project

---
+++ app/src/main/java/com/example/finschoolapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.finschoolapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login_first_activity)
+    }
+}
