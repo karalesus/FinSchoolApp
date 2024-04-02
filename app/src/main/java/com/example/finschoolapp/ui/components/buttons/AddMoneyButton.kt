585a96fb4c99b527bb2cde7d011d090faec4b24a
vvikobra <viktoria.obrokova@yandex.ru>
2024-04-02 16:27:47 +0300

    update AddMoneyButton

--- app/src/main/java/com/example/finschoolapp/ui/components/buttons/AddMoneyButton.kt
+++ app/src/main/java/com/example/finschoolapp/ui/components/buttons/AddMoneyButton.kt
@@ -1,23 +1,22 @@
 package com.example.finschoolapp.ui.components.buttons
 
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.finschoolapp.R
-import com.example.finschoolapp.ui.theme.ThemeColors
 
 @Preview
 @Composable
 fun AddMoney(
 ){
-    IconButton(onClick = { /*TODO*/ },
-    ) {Icon(
-        painter = painterResource(id = R.drawable.add_money_btn),
-        contentDescription = "Добавить деньги в копилку",
-        tint = ThemeColors.LightTheme.secondary
-    )
+    Box() {
+        Image(painter = painterResource(id = R.drawable.add_money_btn),
+            contentDescription = "Добавить деньги в копилку",
+            modifier = Modifier.clickable {  })
     }
 }
 
