9f575e503cb9f4dafac715043a0b6ccda0c92d7a
anvdmal <alekseeva.ann04@mail.ru>
2024-04-03 12:12:09 +0300

    update Color.kt

--- app/src/main/java/com/example/finschoolapp/ui/theme/Color.kt
+++ app/src/main/java/com/example/finschoolapp/ui/theme/Color.kt
@@ -8,17 +8,23 @@ val thirdLight: Color = Color(0xFFE0E0C6)
 val third: Color = Color(0xFFA9A969)
 val secondary: Color = Color(0xFF545A21)
 val lessonBackground: Color = Color(0xFFFAFAF3)
+val moduleBackground: Color = Color(0xFF386667)
+val moduleBackgroundDark: Color = Color(0xFF1E4E4F)
 
 sealed class ThemeColors (
     val background: Color,
     val thirdLight: Color,
     val third: Color,
     val secondary: Color,
-    val lessonBackground : Color) {
+    val lessonBackground : Color,
+    val moduleBackground: Color,
+    val moduleBackgroundDark: Color) {
     object LightTheme : ThemeColors (
         background = mainBackground,
         thirdLight = thirdLight,
         third = third,
         secondary = secondary,
-        lessonBackground = lessonBackground)
+        lessonBackground = lessonBackground,
+        moduleBackground = moduleBackground,
+        moduleBackgroundDark = moduleBackgroundDark)
 }
