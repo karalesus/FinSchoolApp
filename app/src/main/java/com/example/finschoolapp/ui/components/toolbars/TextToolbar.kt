61bcc41fd9530757f04f60d6fd45ee3d90e16c04
anvdmal <alekseeva.ann04@mail.ru>
2024-04-03 12:10:36 +0300

    add text toolbar compose

---
+++ app/src/main/java/com/example/finschoolapp/ui/components/toolbars/TextToolbar.kt
@@ -0,0 +1,54 @@
+package com.example.finschoolapp.ui.components.toolbars
+
+import android.annotation.SuppressLint
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.finschoolapp.ui.theme.ThemeColors
+import com.example.finschoolapp.ui.theme.mainHeader
+
+@OptIn(ExperimentalMaterial3Api::class)
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Composable
+fun TextToolbar(
+    text : String,
+    titleColor : Color,
+    backgroundColor : Color
+) {
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                modifier = Modifier.shadow(7.dp),
+                title = {
+                Text(
+                    text = text,
+                    style = mainHeader,
+                    textAlign = TextAlign.Center)
+            },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = backgroundColor,
+                    titleContentColor = titleColor)
+            )
+        })
+    {
+    }
+}
+
+@Preview
+@Composable
+fun TextToolbarPreview() {
+    val palette = ThemeColors.LightTheme
+    TextToolbar(
+        text = "Ура!",
+        titleColor = palette.secondary,
+        backgroundColor = palette.thirdLight)
+}
