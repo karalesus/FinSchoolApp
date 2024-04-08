b68bbe00a9f474aaf4608cf817e7938a6d7ca302
karalesus <karalesus@gmail.com>
2024-04-08 20:39:58 +0300

    add progressScreen.kt

---
+++ app/src/main/java/com/example/finschoolapp/presentatons/screens/main/ProgressScreen.kt
@@ -0,0 +1,38 @@
+package com.example.finschoolapp.presentatons.screens.main
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.example.finschoolapp.ui.theme.ThemeColors
+
+@Composable
+fun ProgressScreen(
+    modifier: Modifier = Modifier,
+) {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ){
+        Text (
+            text = "Прогресс",
+            color = ThemeColors.Theme.secondary,
+            fontWeight = FontWeight.Bold
+        )
+    }
+}
+
+@Composable
+@Preview
+fun ProgressScreenPreview() {
+    ProgressScreen()
+}
