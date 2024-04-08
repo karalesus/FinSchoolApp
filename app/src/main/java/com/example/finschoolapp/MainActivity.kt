a0167b86441c6553785033bad33613be115ad7c5
karalesus <karalesus@gmail.com>
2024-04-08 20:20:50 +0300

    add scaffold into MainActivity

--- app/src/main/java/com/example/finschoolapp/MainActivity.kt
+++ app/src/main/java/com/example/finschoolapp/MainActivity.kt
@@ -1,13 +1,42 @@
 package com.example.finschoolapp
 
-import android.content.ContentValues.TAG
 import android.os.Bundle
-import android.util.Log
-import androidx.appcompat.app.AppCompatActivity
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
 
-class MainActivity : AppCompatActivity() {
+import com.example.finschoolapp.navigation.graphs.RootNavigationGraph
+import com.example.finschoolapp.navigation.navBars.BottomNavigationBar
+
+
+class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.login_first_activity)
+        setContent {
+            val navController = rememberNavController()
+            var isNavigationBarVisible by remember { mutableStateOf(false) }
+
+            Scaffold(
+                bottomBar = {
+                    if (isNavigationBarVisible) {
+                        BottomNavigationBar(navController = navController)
+                    }
+                }
+            )
+            { padding ->
+                RootNavigationGraph(
+                    paddingValues = padding,
+                    isNavigationBarVisible = { isNavigationBarVisible = it },
+                    navController = navController
+                )
+            }
+        }
     }
 }
