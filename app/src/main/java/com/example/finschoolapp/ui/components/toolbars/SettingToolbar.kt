8b22691b106d7bd4260a2de29e7e7c9974fce7ba
vvikobra <viktoria.obrokova@yandex.ru>
2024-04-12 18:08:24 +0300

    update SettingToolbar.kt

--- app/src/main/java/com/example/finschoolapp/ui/components/toolbars/SettingToolbar.kt
+++ app/src/main/java/com/example/finschoolapp/ui/components/toolbars/SettingToolbar.kt
@@ -6,13 +6,11 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -27,39 +25,32 @@ fun SettingToolbar(
     text: String,
     palette: ThemeColors,
 ) {
-    Scaffold(
-        topBar = {
-            TopAppBar(
-                modifier = Modifier.shadow(7.dp),
-                title = {
-                    Text(
-                        text = text,
-                        style = mainHeader,
-                        modifier = Modifier.padding(start = 16.dp)
-                    )
-                },
-                colors = TopAppBarDefaults.topAppBarColors(
-                    containerColor = palette.thirdLight,
-                    titleContentColor = palette.secondary
-                ),
-                actions = {
-                    IconButton(
-                        onClick = { /*TODO*/ },
-                        modifier = Modifier
-                            .padding(end = 16.dp)
-                            .size(55.dp)
-                    ) {
-                        Icon(
-                            painter = painterResource(id = R.drawable.settings),
-                            contentDescription = "Настройки",
-                            tint = palette.secondary
-                        )
-                    }
-                }
+    TopAppBar(
+        title = {
+            Text(
+                text = text,
+                style = mainHeader,
             )
-        })
-    {
-    }
+        },
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = palette.thirdLight,
+            titleContentColor = palette.secondary
+        ),
+        actions = {
+            IconButton(
+                onClick = { /*TODO*/ },
+                modifier = Modifier
+                    .padding(end = 16.dp)
+                    .size(55.dp)
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.settings),
+                    contentDescription = "Настройки",
+                    tint = palette.secondary
+                )
+            }
+        }
+    )
 }
 
 @Preview
