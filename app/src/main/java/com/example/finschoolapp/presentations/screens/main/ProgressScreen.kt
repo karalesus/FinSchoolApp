e4fa983053f13011e0978a916bf3b54dd4acb434
anvdmal <alekseeva.ann04@mail.ru>
2024-04-14 17:36:11 +0300

    add SettingToolbar to ProgressScreen.kt

--- app/src/main/java/com/example/finschoolapp/presentations/screens/main/ProgressScreen.kt
+++ app/src/main/java/com/example/finschoolapp/presentations/screens/main/ProgressScreen.kt
@@ -1,33 +1,49 @@
 package com.example.finschoolapp.presentations.screens.main
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.example.finschoolapp.ui.components.toolbars.SettingToolbar
 import com.example.finschoolapp.ui.theme.ThemeColors
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun ProgressScreen(
-    modifier: Modifier = Modifier,
+    navController: NavHostController
 ) {
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
-    ){
-        Text (
-            text = "Прогресс",
-            color = ThemeColors.LightTheme.secondary,
-            fontWeight = FontWeight.Bold
-        )
+    Scaffold (
+        topBar = {
+            SettingToolbar(
+                text = "Прогресс",
+                palette = ThemeColors.LightTheme,
+                navController = navController
+            )
+        }
+    ) {
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(
+                text = "Прогресс",
+                color = ThemeColors.LightTheme.secondary,
+                fontWeight = FontWeight.Bold
+            )
+        }
     }
 }
 
 @Composable
 @Preview
 fun ProgressScreenPreview() {
-    ProgressScreen()
+    ProgressScreen(navController = rememberNavController())
 }
