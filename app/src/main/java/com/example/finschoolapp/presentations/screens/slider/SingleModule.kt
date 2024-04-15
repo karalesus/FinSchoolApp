2d3b24615d80ced0f5b658dd7f5b19655b73ccce
karalesus <karalesus@gmail.com>
2024-04-15 11:12:57 +0300

    delete unused imports in ContinueModuleButton, SignUpScreen, SingleModule, ThemeSlider

--- app/src/main/java/com/example/finschoolapp/presentations/screens/slider/SingleModule.kt
+++ app/src/main/java/com/example/finschoolapp/presentations/screens/slider/SingleModule.kt
@@ -4,10 +4,8 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -16,9 +14,7 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import com.example.finschoolapp.ui.theme.smallDescription
 import com.example.finschoolapp.ui.theme.smallDescriptionLight
-import com.example.finschoolapp.ui.theme.smallHeader
 import com.example.finschoolapp.ui.theme.smallHeaderLight
 
 
--- app/src/main/java/com/example/finschoolapp/ui/components/buttons/ContinueModuleButton.kt
+++ app/src/main/java/com/example/finschoolapp/ui/components/buttons/ContinueModuleButton.kt
@@ -2,7 +2,6 @@ package com.example.finschoolapp.ui.components.buttons
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
@@ -10,10 +9,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.finschoolapp.R
 import com.example.finschoolapp.ui.theme.LocalDimensions
 import com.example.finschoolapp.ui.theme.ThemeColors
 import com.example.finschoolapp.ui.theme.textViewBaseVariant
