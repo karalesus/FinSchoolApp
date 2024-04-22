c21f7ca201ad4737567ba3acf1ea6862aef405a4
OTCLICK <ivan.budko.2004@mail.ru>
2024-04-22 19:05:18 +0300

    beginning of the ViewModel

--- app/src/main/java/com/example/finschoolapp/MainActivity.kt
+++ app/src/main/java/com/example/finschoolapp/MainActivity.kt
@@ -3,19 +3,26 @@ package com.example.finschoolapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 
 import com.example.finschoolapp.navigation.graphs.RootNavigationGraph
 import com.example.finschoolapp.navigation.navBars.BottomNavigationBar
+import com.example.finschoolapp.presentations.viewmodels.MainViewModel
 
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,5 +45,7 @@ class MainActivity : ComponentActivity() {
                 )
             }
         }
+        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+        viewModel.mainState.observe(this, Observer{newState ->})
     }
 }
---
+++ app/src/main/java/com/example/finschoolapp/presentations/viewmodels/MainViewModel.kt
@@ -0,0 +1,15 @@
+package com.example.finschoolapp.presentations.viewmodels
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+
+    private val privateMainState = MutableLiveData<String>()
+    val mainState: LiveData<String> get() = privateMainState
+
+    fun updateState(newState: String) {
+        privateMainState.value = newState
+    }
+}
