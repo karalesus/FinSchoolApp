e14940a30c970340bc752b6fbb1ba9aca4683571
anvdmal <alekseeva.ann04@mail.ru>
2024-10-19 19:43:34 +0300

    add fourth mini-game main screen

---
+++ app/src/main/java/com/example/finschoolapp/FourthMiniGameMainScreen.kt
@@ -0,0 +1,83 @@
+package com.example.finschoolapp
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.example.finschoolapp.navigation.main.MainScreen
+import com.example.finschoolapp.ui.components.buttons.PrimaryButton
+import com.example.finschoolapp.ui.components.toolbars.TextToolbar
+import com.example.finschoolapp.ui.theme.LocalDimensions
+import com.example.finschoolapp.ui.theme.ThemeColors
+import com.example.finschoolapp.ui.theme.smallHeader
+
+@Composable
+fun FourthMiniGameMainScreen(
+    modifier: Modifier = Modifier,
+    navController: NavHostController
+) {
+    val dimensions = LocalDimensions.current
+    val palette = ThemeColors.LightTheme
+
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(color = palette.background),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        TextToolbar(
+            text = stringResource(id = R.string.label_fourth_mini_game),
+            titleColor = palette.secondary,
+            backgroundColor = palette.thirdLight
+
+        )
+
+        Spacer(modifier = modifier.height(dimensions.verticalXLarge))
+
+        Image(
+            painter = painterResource(id = R.drawable.fourth_mini_game_main),
+            contentDescription = "The icon of the fourth mini-game",
+            modifier = modifier
+                .size(300.dp)
+        )
+
+        Spacer(modifier = modifier.height(dimensions.verticalSLarge))
+
+        Text(
+            text = stringResource(id = R.string.task_fourth_mini_game),
+            style = smallHeader.copy(color = palette.secondary),
+            textAlign = TextAlign.Center
+        )
+
+        Spacer(modifier = modifier.height(dimensions.verticalSLarge))
+
+        PrimaryButton(
+            modifier = modifier.size
+                (width = 300.dp, height = 35.dp),
+            palette = ThemeColors.LightTheme,
+            text = stringResource(id = R.string.button_continue),
+            navController = navController,
+            route = MainScreen.AddGoal.route
+        )
+    }
+}
+
+@Preview
+@Composable
+fun FourthMiniGameMainScreenPreview() {
+    FourthMiniGameMainScreen(navController = rememberNavController())
+}
