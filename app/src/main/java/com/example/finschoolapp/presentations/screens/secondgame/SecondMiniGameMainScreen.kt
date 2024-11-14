39066108b7149cd303f30ac14bd5ed833bb52e91
vvikobra <viktoria.obrokova@yandex.ru>
2024-11-14 12:55:23 +0300

    delete unused import

--- app/src/main/java/com/example/finschoolapp/presentations/screens/secondgame/SecondMiniGameMainScreen.kt
+++ app/src/main/java/com/example/finschoolapp/presentations/screens/secondgame/SecondMiniGameMainScreen.kt
@@ -11,7 +11,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -30,8 +29,6 @@ import com.example.finschoolapp.ui.components.buttons.PrimaryButton
 import com.example.finschoolapp.ui.components.toolbars.TextToolbar
 import com.example.finschoolapp.ui.theme.LocalDimensions
 import com.example.finschoolapp.ui.theme.ThemeColors
-import com.example.finschoolapp.ui.theme.smallDescription
-import com.example.finschoolapp.ui.theme.smallHeader
 import com.example.finschoolapp.ui.theme.textViewBaseVariant
 
 @Composable
