f8f98f8621294c6aab141385f739563727103604
karalesus <karalesus@gmail.com>
2024-11-28 12:17:39 +0300

    add ArticleScreen with routes for navigation

---
+++ app/src/main/java/com/example/finschoolapp/navigation/articles/ArticleScreen.kt
@@ -0,0 +1,10 @@
+package com.example.finschoolapp.navigation.articles
+
+
+import androidx.compose.runtime.Immutable;
+
+@Immutable
+sealed class ArticleScreen (val route: String) {
+    object ArticleList: ArticleScreen("ArticleList")
+    object ArticleDetail: ArticleScreen("ArticleDetail")
+}
