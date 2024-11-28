47089e6b0d1018efe5a618d4479e0c69bb70c554
karalesus <karalesus@gmail.com>
2024-11-28 12:14:04 +0300

    add articlesNavGraph in root nav graph

--- app/src/main/java/com/example/finschoolapp/navigation/graphs/RootNavGraph.kt
+++ app/src/main/java/com/example/finschoolapp/navigation/graphs/RootNavGraph.kt
@@ -7,6 +7,7 @@ import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import com.example.finschoolapp.navigation.RootScreen
+import com.example.finschoolapp.navigation.articles.articlesNavGraph
 import com.example.finschoolapp.navigation.main.mainScreenGraph
 
 @Composable
@@ -14,10 +15,11 @@ fun RootNavigationGraph(
     paddingValues: PaddingValues,
     navController: NavHostController,
     isNavigationBarVisible: (Boolean) -> Unit,
-){
+) {
+
     NavHost(
         modifier = Modifier.padding(paddingValues = paddingValues),
-        navController  = navController,
+        navController = navController,
         startDestination = RootScreen.AuthNavGraph.route
     ) {
         authNavGraph(navController = navController) {
@@ -26,13 +28,8 @@ fun RootNavigationGraph(
         mainScreenGraph(navController = navController) {
             isNavigationBarVisible(true)
         }
-
+        articlesNavGraph(navController = navController) {
+            isNavigationBarVisible(true)
+        }
     }
-}
-
-object Graph {
-    const val ROOT = "root_graph"
-    const val AUTHENTICATION = "auth_graph"
-    const val PROGRESS = "progress_auth"
-    // TODO: дальнейшие переходы на экраны
 }
