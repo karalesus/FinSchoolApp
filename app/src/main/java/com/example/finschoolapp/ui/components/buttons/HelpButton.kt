24668bc84e6456175cf722794038175f89d5ac2f
“Nastya” <nastya.cap04@gmail.com>
2024-12-04 13:25:53 +0300

    add buttons and screens for six game

--- app/src/main/java/com/example/finschoolapp/ui/components/buttons/HelpButton.kt
+++ app/src/main/java/com/example/finschoolapp/ui/components/buttons/HelpButton.kt
@@ -3,6 +3,7 @@ package com.example.finschoolapp.ui.components.buttons
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.finschoolapp.R
@@ -11,13 +12,15 @@ import com.example.finschoolapp.ui.theme.ThemeColors
 @Preview
 @Composable
 fun HelpButton(
-){
-    IconButton(onClick = { /*TODO*/ },
-    ) { Icon(
-        painter = painterResource(id = R.drawable.help_btn),
-        contentDescription = null,
-        tint = ThemeColors.LightTheme.secondary
-    )
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit = {}
+) {
+    IconButton(onClick = onClick, modifier = modifier) {
+        Icon(
+            painter = painterResource(id = R.drawable.help_btn),
+            contentDescription = null,
+            tint = ThemeColors.LightTheme.secondary
+        )
     }
 }
 
