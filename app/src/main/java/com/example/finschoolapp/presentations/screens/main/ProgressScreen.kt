a0d97b04543a933ed55cece3b2c299468eacb8a8
karalesus <karalesus@gmail.com>
2024-12-05 12:32:40 +0300

    fix progressScreen

--- app/src/main/java/com/example/finschoolapp/presentations/screens/main/ProgressScreen.kt
+++ app/src/main/java/com/example/finschoolapp/presentations/screens/main/ProgressScreen.kt
@@ -1,6 +1,5 @@
 package com.example.finschoolapp.presentations.screens.main
 
-import android.annotation.SuppressLint
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
@@ -15,9 +14,6 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Scaffold
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -29,18 +25,12 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.example.finschoolapp.R
-import com.example.finschoolapp.navigation.navBars.BottomNavigationBar
 import com.example.finschoolapp.ui.components.ProgressLine.ProgressLine
 import com.example.finschoolapp.ui.components.buttons.SaveThemeButton
 import com.example.finschoolapp.ui.components.toolbars.SettingToolbar
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
-import com.example.finschoolapp.navigation.main.MainScreen
-import com.example.finschoolapp.ui.components.buttons.PrimaryButton
 import com.example.finschoolapp.ui.theme.ThemeColors
 import com.example.finschoolapp.ui.theme.mainBackground
 import com.example.finschoolapp.ui.theme.moduleBackground
@@ -60,41 +50,31 @@ fun ProgressScreen(
                 palette = ThemeColors.LightTheme,
                 navController = navController
             )
-        },
-        bottomBar = {
-            BottomNavigationBar(
-                modifier = Modifier,
-                navController = navController,
-                palette = ThemeColors.LightTheme
-            )
         }
     ) { innerPadding ->
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(innerPadding) // Учитываем отступы, чтобы избежать наложения
+                .padding(innerPadding) // отступы, чтобы избежать наложения
                 .background(color = palette.background),
         ) {
             Box(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .weight(0.2f) // Занимает оставшееся пространство
+                    .weight(0.2f) // оставшееся пространство
             ) {
                 Column(
-                    verticalArrangement = Arrangement.spacedBy(5.dp), // Устанавливаем расстояние между элементами
+                    verticalArrangement = Arrangement.spacedBy(5.dp), // расстояние между элементами
                     modifier = Modifier
                         .align(Alignment.CenterStart)
                         .padding(20.dp)
                 )
                 {
-
                     Text(
-                        //modifier = Modifier.padding(10.dp),
                         text = stringResource(id = R.string.label_username),
                         style = textViewBaseVariant.copy(color = palette.secondary)
                     )
                     Text(
-                        // modifier = Modifier.padding(10.dp),
                         text = stringResource(id = R.string.label_level),
                         style = smallHeader.copy(color = palette.secondary, fontSize = 16.sp)
                     )
@@ -105,7 +85,6 @@ fun ProgressScreen(
                     modifier = Modifier
                         .size(100.dp)
                         .align(Alignment.CenterEnd)
-
                 )
 
                 Image(
@@ -189,9 +168,6 @@ fun ProgressScreen(
         }
     }
 }
-
-
-
 
 @Composable
 @Preview
