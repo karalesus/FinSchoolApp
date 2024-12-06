30d284751dbeb6a4af4e8de0f32c5714b2918e0e
karalesus <karalesus@gmail.com>
2024-12-06 14:51:33 +0300

    delete 5th game

--- app/src/main/java/com/example/finschoolapp/navigation/RootScreen.kt
+++ app/src/main/java/com/example/finschoolapp/navigation/RootScreen.kt
@@ -5,7 +5,6 @@ import androidx.compose.runtime.Immutable
 @Immutable
 sealed class RootScreen
     (val route: String) {
-    object FifthGameNavGraph:RootScreen("FifthGame")
     object AuthNavGraph : RootScreen("AuthNavGraph")
     object MainScreenGraph : RootScreen("MainScreenGraph")
     object ArticlesNavGraph : RootScreen("ArticlesNavGraph")
